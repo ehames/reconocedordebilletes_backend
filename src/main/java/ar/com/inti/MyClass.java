5d5b261b60962754fc6d1ba2315389189ec1b135
Ivan Lagiglia <lagigliaivan@gmail.com>
2016-04-13 13:08:12 -0300

    adding pom file

---
+++ src/main/java/ar/com/inti/MyClass.java
@@ -0,0 +1,11 @@
+package ar.com.inti;
+
+/**
+ * Created by ivan on 13/04/16.
+ */
+public class MyClass {
+
+    public void test(){
+
+    }
+}
---
+++ src/test/java/ar/com/inti/MyTest.java
@@ -0,0 +1,16 @@
+package ar.com.inti;
+
+import org.junit.Test;
+
+/**
+ * Created by ivan on 13/04/16.
+ */
+
+public class MyTest {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
